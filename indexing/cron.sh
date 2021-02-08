dddba1c081368f2bd42af3282dec4595ee25c612
Bo Henriksen <stumpdenmark@hotmail.com>
2021-02-08 11:53:45 +0100

    Update cron.sh
    Use date when displaying status

--- indexing/cron.sh
+++ indexing/cron.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
+echo "In production, running index scripts now";
 
-echo "running erindringer.py";
-/usr/local/bin/python3 /usr/src/app/erindringer.py > /proc/$$/fd/1;
-echo "running efterretninger.py";
-/usr/local/bin/python3 /usr/src/app/efterretninger.py > /proc/$$/fd/1;
+echo "$(date): running erindringer.py" >> /var/log/cron.log 2>&1;
+/usr/local/bin/python3 /usr/src/app/erindringer.py >> /var/log/cron.log 2>&1;
+echo "$(date): running efterretninger.py" >> /var/log/cron.log 2>&1;
+/usr/local/bin/python3 /usr/src/app/efterretninger.py >> /var/log/cron.log 2>&1;
 #echo "running schools.py";
 #/usr/local/bin/python3 /usr/src/app/schools.py > /proc/1/fd/1;
-echo "running burials.py";
-/usr/local/bin/python3 /usr/src/app/burials.py > /proc/$$/fd/1;
-echo "running police.py";
-/usr/local/bin/python3 /usr/src/app/police.py > /proc/$$/fd/1;
+echo "$(date): running burials.py" >> /var/log/cron.log 2>&1;
+/usr/local/bin/python3 /usr/src/app/burials.py >> /var/log/cron.log 2>&1;
+echo "$(date): running police.py" >> /var/log/cron.log 2>&1;
+/usr/local/bin/python3 /usr/src/app/police.py >> /var/log/cron.log 2>&1;
+#
