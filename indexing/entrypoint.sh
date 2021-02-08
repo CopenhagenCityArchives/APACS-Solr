07b6184b9044c4ba1800928e2ece1a06af5d7dcf
Bo Henriksen <stumpdenmark@hotmail.com>
2021-02-08 11:53:32 +0100

    Create entrypoint.sh

---
+++ indexing/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Start the run once job.
+echo "Docker container has been started. Adding cron job to scheduler.txt"
+
+# Setup a cron schedule
+echo "0 1 * * 0,2-6 INDEX_DELETE=false root /usr/src/app/cron.sh >> /var/log/cron.log 2>&1
+0 1 * * 1 INDEX_DELETE=true root /usr/src/app/cron.sh >> /var/log/cron.log 2>&1
+# This extra line makes it a valid cron" > scheduler.txt
+
+echo "scheduler.txt: "
+cat sceduler.txt
+
+crontab scheduler.txt
+
+echo "crontab -l:"
+crontab -l
+
+cron -f
