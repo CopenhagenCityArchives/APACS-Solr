ce971385df5aa5a5ef14657474aacc440e1b3b45
Bo Henriksen <stumpdenmark@hotmail.com>
2021-02-09 10:35:51 +0100

    Update entrypoint.sh
    Save env variables in txt-file
    Remove user in cron job definition

--- indexing/entrypoint.sh
+++ indexing/entrypoint.sh
@@ -3,17 +3,21 @@
 # Start the run once job.
 echo "Docker container has been started. Adding cron job to scheduler.txt"
 
+echo "Retrieving enviromental variables and saving them"
+env > /usr/src/app/env.txt
+
 # Setup a cron schedule
-echo "0 1 * * 0,2-6 INDEX_DELETE=false root /usr/src/app/cron.sh >> /var/log/cron.log 2>&1
-0 1 * * 1 INDEX_DELETE=true root /usr/src/app/cron.sh >> /var/log/cron.log 2>&1
+echo "0 1 * * 0,2-6 INDEX_DELETE=false /usr/src/app/cron.sh >> /var/log/cron.log 2>&1
+0 1 * * 1 INDEX_DELETE=true /usr/src/app/cron.sh >> /var/log/cron.log 2>&1
 # This extra line makes it a valid cron" > scheduler.txt
 
-echo "scheduler.txt: "
+echo "Running cat scheduler.txt:"
 cat scheduler.txt
 
 crontab scheduler.txt
 
-echo "crontab -l:"
+echo "Running crontab -l"
 crontab -l
 
+echo "Running cron -f"
 cron -f
