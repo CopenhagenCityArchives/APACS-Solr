8e34746c84362220fe7eeb2765d812478885b97c
Bo Henriksen <stumpdenmark@hotmail.com>
2021-06-02 23:39:20 +0200

    Update police.py
    Order by registerblad.id instead of person.registerblad_id. This avoids a very expensive file_sort!

--- indexing/police.py
+++ indexing/police.py
@@ -17,8 +17,6 @@ class PoliceIndexer(IndexerBase):
 
         self.count_query = "SELECT COUNT(*) as count FROM PRB_person"
 
-        # note that an expensive order by is needed in order to couple
-        # persons on same registerblad while chunking the results in smaller sizes!
         self.person_query = """
     SELECT
     ae.tasks_id as task_id,
@@ -69,7 +67,7 @@ FROM
     LEFT JOIN apacs_posts apost ON ae.posts_id = apost.id
     LEFT JOIN apacs_pages apage on apost.pages_id = apage.id
     WHERE ae.tasks_id = 5
-    ORDER BY p.registerblad_id
+    ORDER BY r.id
     LIMIT %d, %d
 """
 
